41918e6ed5af8595b029c99ae3287f0a487f24fb
223368 <223368@student.upm.edu.my>
2025-06-29 14:45:51 +0800

    Initial commit

---
+++ src/main/java/com/csc3700/agroguard/AgroGuardApplication.java
@@ -0,0 +1,13 @@
+package com.csc3700.agroguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc3700/agroguard/AgroGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc3700.agroguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
